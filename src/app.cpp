4c515f9fe73046d52a8e811f2a542943aa977363
emorris3 <emorris3@ucmerced.edu>
2019-11-22 23:52:06 -0800

    Created and set the values for the 3 vectors.

--- src/app.cpp
+++ src/app.cpp
@@ -8,17 +8,29 @@ int main(int argc, char** argv){
     CrowServer server(argc, argv);
 
     server.renderHTML("/", "index.html");
+    
+    // create 3 unordered maps
+    std::unordered_map<std::string, double> band;
+    std::unordered_map<std::string, double> multiplier;
+    std::unordered_map<std::string, std::string> tolerance;
 
+    // Set the values for each of the unordered maps
+    // Set band values here
+    band["Black"] = 0, band["Brown"] = 1, band["Red"] = 2;
+    band["Orange"] = 3, band["Yellow"] = 4, band["Green"] = 5;
+    band["Blue"] = 6, band["Violet"] = 7, band["Grey"] = 8, band["White"] = 9;
+    // Set multiplier values here
+    multiplier["Black"] = 1, multiplier["Brown"] = 10, multiplier["Red"] = 100;
+    multiplier["Orange"] = 1000, multiplier["Yellow"] = 10000, multiplier["Green"] = 100000;
+    multiplier["Blue"] = 1000000, multiplier["Violet"] = 10000000, multiplier["Gold"] = .1, multiplier["Silver"] = .01;
+    // Set tolerance values here
+    tolerance["Brown"] = " +/- 1% ", tolerance["Red"] = " +/- 2% ", tolerance["Green"] = " +/- .5% ";
+    tolerance["Blue"] = " +/- .25% ", tolerance["Violet"] = " +/- .1% ", tolerance["Grey"] = " +/- .05% ";
+    tolerance["Gold"] = " +/- 5% ", tolerance["Silver"] = " +/- 10% ";
 
     server.route("/resistance", [&](const request& req, response& res){
 
         if (req.has_params({"b1", "b2", "b3", "b4", "b5"})){
-            // create 3 unordered maps
-            std::unordered_map<std::string, double> band;
-            std::unordered_map<std::string, double> multiplier;
-            std::unordered_map<std::string, std::string> tolerance;
-
-
             // Get the inputs for each of the buttons
             std::string button_One = req.url_params.get("b1");
             std::string button_Two = req.url_params.get("b2");
